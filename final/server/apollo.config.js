3f80d7a4cd4bc13e0b84016bbad3f8ecfe92f8e6
Pete Naish <pete@nai.sh>
2019-09-17 15:50:16 +0100

    build everything

--- final/server/apollo.config.js
+++ final/server/apollo.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   service: {
-    name: 'space-explorer',
+    name: 'pete-naish-6005',
   },
 };
---
+++ start/client/src/index.js
@@ -0,0 +1,33 @@
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from '@apollo/react-hooks';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pages from './pages';
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'http://localhost:4000',
+  headers: {
+    authorization: localStorage.getItem('token'),
+  },
+});
+
+const client = new ApolloClient({
+  cache,
+  link,
+});
+
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem('token'),
+    cartItems: [],
+  },
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Pages />
+  </ApolloProvider>, document.getElementById('root')
+);
